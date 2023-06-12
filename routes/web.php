b822afa801bd687f538c43b2fec00686a8a23ce3
Naufaly Zuhdi Indriandi <naufal.zi2620@gmail.com>
2023-06-12 23:21:52 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -46,41 +46,35 @@ Route::get('/details', function () {
     return view('userpage.page.productdetails');
 });
 // Adminpage
-Route::get('/login', function () {
-    return view('adminpage.page.login');
-});
+Route::group(['middleware' => ['admin']], function () {
+    Route::get('/login', function () {
+        return view('adminpage.page.login');
+    });
+    
+    Route::get('/homepage', function () {
+        return view('adminpage.page.homepage');
+    });
+    
+    Route::get('/category', [MenuCategoryController::class, 'get_all_onadmin']);
+    Route::post('/add_menu_category', [MenuCategoryController::class, 'add_menu_category']);
+    Route::post('/edit_menu_category', [MenuCategoryController::class, 'edit_menu_category']);
+    Route::post('/delete_menu_category/{id}', [MenuCategoryController::class, 'delete_menu_category']);
+    Route::get('/products', [ProductController::class, 'show_products_onadmin']);
+    Route::get('/table', [TableController::class, 'show_tables_onadmin']);
 
-Route::get('/homepage', function () {
-    return view('adminpage.page.homepage');
-});
-
-Route::get('/category', [MenuCategoryController::class, 'get_all_onadmin']);
-Route::post('/add_menu_category', [MenuCategoryController::class, 'add_menu_category']);
-Route::post('/edit_menu_category', [MenuCategoryController::class, 'edit_menu_category']);
-Route::post('/delete_menu_category/{id}', [MenuCategoryController::class, 'delete_menu_category']);
-Route::get('/products', [ProductController::class, 'show_products_onadmin']);
-Route::get('/table', [TableController::class, 'show_tables_onadmin']);
-
-// Route::get('/products', function () {
-//     return view('adminpage.page.products');
-// });
-
-// Route::get('/table', function () {
-//     return view('adminpage.page.table');
-// });
-
-Route::get('/order', function () {
-    return view('adminpage.page.order');
-});
-
-Route::get('/detailorder/incoming', function () {
-    return view('adminpage.page.detailorder-incoming');
-});
-
-Route::get('/detailorder/all', function () {
-    return view('adminpage.page.detailorder-allorder');
-});
-
-Route::get('/admin', function () {
-    return view('adminpage.page.admin');
+    Route::get('/order', function () {
+        return view('adminpage.page.order');
+    });
+    
+    Route::get('/detailorder/incoming', function () {
+        return view('adminpage.page.detailorder-incoming');
+    });
+    
+    Route::get('/detailorder/all', function () {
+        return view('adminpage.page.detailorder-allorder');
+    });
+    
+    Route::get('/admin', function () {
+        return view('adminpage.page.admin');
+    });
 });
